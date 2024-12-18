3c790495ea1c6ab79312e21841c9d5a4b65c4e1d
Sean Zhang <syanix@gmail.com>
2024-12-18 14:28:34 +1100

    Migrate project to astro based

---
+++ astro.config.mjs
@@ -0,0 +1,22 @@
+// @ts-check
+// @ts-check
+import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
+import cloudflare from '@astrojs/cloudflare';
+
+// https://astro.build/config
+export default defineConfig({
+  output: 'server', // Enable SSR for Cloudflare Pages
+  site: 'https://www.viabrisbane.com', // Replace with your site URL
+  integrations: [sitemap()],
+  adapter: cloudflare({
+    platformProxy: {
+      enabled: true,
+    }
+  }),
+  vite: {
+    build: {
+      target: 'esnext',
+    },
+  },
+});
